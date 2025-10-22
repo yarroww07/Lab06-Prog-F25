0393f01a55da7da45a16d5d616c68e160e510b83
Meriemhamzi <meriem.hamzi@outlook.com>
2025-10-22 11:09:58 -0400

    Create Questions.py

---
+++ Questions.py
@@ -0,0 +1,6 @@
+# Question 1: What is the average international tourism per region?
+# Question 2: Which sex (male vs female) has the highest life expectancy in Africa?
+# Question 3: Which intermediate region has the highest GNI?
+# Question 4: 
+# Question 5:
+# Question 6:
