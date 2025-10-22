e3997366d4bb1b37aca103f4e775c29cc31ae4e4
yarroww07 <raniabouladraf@gmail.com>
2025-10-22 11:33:10 -0400

    um

--- Questions.py
+++ Questions.py
@@ -1,6 +1,9 @@
 # Question 1: What is the average international tourism per region?
 # Question 2: Which sex (male vs female) has the highest life expectancy in Africa?
 # Question 3: Which intermediate region has the highest GNI?
-# Question 4: 
-# Question 5:
-# Question 6:
+# Question 4: Do high income countries produce more greenhouse gas emissions?
+# Question 5: Which continents are the most visited by tourists? Region? Subregion?
+# Question 6: How is the number of physicians related to tertiary education?
+# Question 7: Is a higher tertiary education for females related to high income economies? 
+#             To the amount of women in national parlament?
+
