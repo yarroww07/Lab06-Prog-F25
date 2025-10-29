b086e284cc8c0ee7704aabcdca8d658033108389
Meriemhamzi <meriem.hamzi@outlook.com>
2025-10-29 12:04:04 -0400

    Update Lab06.py
    fitered data with loops fixed

--- Lab06.py
+++ Lab06.py
@@ -17,29 +17,33 @@ import seaborn as sns
 
 dataset = pd.read_csv("wdi_wide.csv")
 
-# print(dataset.info())
+print(dataset.info())
  
 # Physician = 10 null values 
 # Population = 0 null values 
 
-# print(dataset.nunique())
-# print(dataset.describe())
+print(dataset.nunique())
+print(dataset.describe())
 # It provides statistical information for each column (count, mean, standard deviation, quartiles, minimum and maximum)
 
 dataset['GNI per capita'] = dataset['GNI']/dataset['Population']
-# print(dataset['GNI per capita'].round())
+print(dataset['GNI per capita'].round())
 
-# print(dataset.value_counts('Region'))
-# print(dataset.value_counts('High Income Economy'))
-# print(pd.crosstab(dataset['Region'], dataset['High Income Economy']))
-# print(dataset.columns)
+print(dataset.value_counts('Region'))
+print(dataset.value_counts('High Income Economy'))
+print(pd.crosstab(dataset['Region'], dataset['High Income Economy']))
+print(dataset.columns)
 
 
-#country_count = 0
-
-#filtered_data = dataset[dataset['Life expectancy, female'] > 80]
-#country_count = filtered_data.sum()
-#print(filtered_data)
+country_count = 0
+filtered_data = []
+for value, row in dataset.iterrows():
+    if row['Life expectancy, female'] > 80:
+        filtered_data += [row['Country Name']]
+        country_count += 1
+print('There are',country_count, 'countries with female life expectancy greater 80:')
+print(filtered_data)
+     
 
 
 
